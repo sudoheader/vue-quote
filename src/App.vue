ece0295147f38ad1c011786b47e00155700f066d
Raymond Nazaryan <rayn2427@gmail.com>
2018-03-30 18:41:30 -0700

    Added Quote.vue and QuoteGrid.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
     export default {
-        
+      data: function () {
+        return {
+          quotes: [
+            'Just a Quote to see something'
+          ],
+          maxQuotes: 10
+        }
+      }
     }
 </script>
 
